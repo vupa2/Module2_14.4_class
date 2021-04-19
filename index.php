a4a17c8ef028304d6dc9f7bf33274c6846d3c3be
vupa2 <anhvu.pa96@gmail.com>
2021-04-19 16:46:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $data = ['C0318G', 'M0318G', 'P0323A'];
 
 function checkClass($str)
 {
-    $regexp = '/^[C|A|P]\d{4}[G-M]/';
+    $regexp = '/^[C|A|P]\d{4}[G-M]$/';
     if (preg_match($regexp, $str)) {
         echo ("$str is a valid class.") . '<br>';
     } else {
